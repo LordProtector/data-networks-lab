04a663f7cb6730651f02f7b68563cbb38f383e8a
JonasB <s9jobuer@stud.uni-saarland.de>
2011-06-30 14:59:36 +0200

    Versuch den Timer richtig zu verwenden, leider crashed der code, bitte testen! (Ich weiß, man soll keinen nicht funktionierenden code commiten, aber ihr wolltet es ja)

--- milestone1.c
+++ milestone1.c
@@ -11,9 +11,25 @@ static EVENT_HANDLER(application_ready)
 
   size_t length = sizeof(msg);
   CHECK(CNET_read_application(&destaddr, msg, &length));
-  CNET_disable_application(ALLNODES);
+  printf(" DATA read from applicaiton: '%s' (%d bytes)\n", msg, length);
+  CNET_disable_application(destaddr);
+  printf("propagationdelay: %d µs\n",linkinfo[link].propagationdelay);
   CHECK(CNET_write_physical(link, msg, &length));
-  //printf(" DATA transmitted: '%s' (%d bytes)\n", msg, length);
+  //calcute how long a frame needs to travel over the link
+  double bandwidth = (double) linkinfo[link].bandwidth / 1000000.;
+  double a = (double)(length*8.) / bandwidth; 
+  double t = a + linkinfo[link].propagationdelay;
+  printf("bandwidth: %f bit/µsec \n", bandwidth);
+  printf("length: %f bit\n", length*8.);
+  printf("a: %f\n", a);
+  printf("timer: %f µsec\n", t);
+  CNET_start_timer(EV_TIMER1, t, 0);
+}
+
+static EVENT_HANDLER(timeout)
+{	
+  printf("timeout\n");
+  CNET_enable_application(ALLNODES);
 }
 
 static EVENT_HANDLER(physical_ready)
@@ -36,4 +52,6 @@ EVENT_HANDLER(reboot_node)
 
     if(nodeinfo.nodenumber == 0)
       CNET_enable_application(ALLNODES);
+
+    CHECK(CNET_set_handler(EV_TIMER1, timeout, 0));
 }
