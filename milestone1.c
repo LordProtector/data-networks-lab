b0c4bcf64133470e1c454293093a640859798e1b
Alexander Bunte <s9albunt@stud.uni-saarland.de>
2011-07-05 10:42:07 +0200

    Code refactoring.

--- milestone1.c
+++ milestone1.c
@@ -15,54 +15,65 @@ char msg[MAX_MESSAGE_SIZE];
 
 bool timer_started;
 
-void set_timeout(size_t length)
+//calcute how long a frame needs to travel over the link
+double transmission_delay(size_t length, int link)
 {
-  printf(" DATA transmitted: %d bytes\n", length);
-  //calcute how long a frame needs to travel over the link
-  double bandwidth = (double) linkinfo[1].bandwidth * MICRO;
-  double transmission_delay = (double) (length * BYTE_LENGTH) / bandwidth; 
-  double t = transmission_delay + LINK_DELAY;
-  CNET_start_timer(EV_TIMER1, t, 0);
+  double bandwidth = (double) linkinfo[link].bandwidth * MICRO;
+  double transmission_delay = (double) (length * BYTE_LENGTH) / bandwidth;
+
+  return transmission_delay;
+}
+
+void transmit(int	link)
+{
+  char *msg;
+  size_t length;
+  double timeout;
+  
+  if (queue_nitems(msgQ)) {
+    msg = queue_peek(msgQ, &length);
+    if (queue_nitems(msgQ) <= QUEUE_MIN_MSGS) {
+      CNET_enable_application(ALLNODES);
+    }
+    int ph_status = CNET_write_physical(link, msg, &length);
+    if (ph_status == ER_TOOBUSY) {
+      // If link is still busy wait another microsecond
+      timeout = 1.;
+    } else {
+      CHECK(ph_status);
+      printf(" DATA transmitted: %d bytes\n", length);
+      msg = queue_remove(msgQ, &length);
+      free(msg);
+      timeout = transmission_delay(length, link) + LINK_DELAY;
+    }
+    CNET_start_timer(EV_TIMER1, timeout, 0);
+    timer_started = true;
+  } else {
+    timer_started = false;
+  }
 }
 
 static EVENT_HANDLER(application_ready)
 {
-  int	link = 1;
   CnetAddr destaddr;
+  int link = 1;
   size_t length = sizeof(msg);
 
   CHECK(CNET_read_application(&destaddr, msg, &length));
-  if (queue_nitems(msgQ) >= QUEUE_MAX_MSGS) CNET_disable_application(ALLNODES);
-  if (timer_started) {
-    queue_add(msgQ, msg, length);
-  } else {
-    CHECK(CNET_write_physical(link, msg, &length));
-    set_timeout(length);
-    timer_started = true;
+  if (queue_nitems(msgQ) >= QUEUE_MAX_MSGS) {
+    CNET_disable_application(ALLNODES);
+  }
+  queue_add(msgQ, msg, length);
+  if (!timer_started) {
+    transmit(link);
   }
 }
 
 static EVENT_HANDLER(link_ready)
 {
-  int	link = 1;
-  size_t length;
-  char *msg;
-
-  if (queue_nitems(msgQ)) {
-    msg = queue_peek(msgQ, &length);
-    if (queue_nitems(msgQ) <= QUEUE_MIN_MSGS) CNET_enable_application(ALLNODES);
-    int ph_status = CNET_write_physical(link, msg, &length);
-    if (ph_status == ER_TOOBUSY) {
-      CNET_start_timer(EV_TIMER1, 1, 0);
-    } else {
-      CHECK(ph_status);
-      set_timeout(length);
-      msg = queue_remove(msgQ, &length);
-      free(msg);
-    }
-  } else {
-    timer_started = false;
-  }
+  int link = 1;
+  
+  transmit(link);
 }
 
 static EVENT_HANDLER(physical_ready)
