040293274ad5423fa2839e02bd9dd27519de9584
JonasB <s9jobuer@stud.uni-saarland.de>
2011-07-05 11:34:32 +0200

    Add documentation to milestone1.c
    Documentation added should be doxygen compatible.

--- milestone1.c
+++ milestone1.c
@@ -1,21 +1,67 @@
+/**
+ * milstone1.c
+ *  
+ * @autors Stefan Tombers, Alexander Bunte, Jonas Bürse
+ *
+ * Implementation of milstone 1 of the data networks lab 2011
+ */
+
+/* include headers */
 #include <stdlib.h>
 #include <string.h>
 #include <cnet.h>
 #include <cnetsupport.h>
 
+/**
+ * Muliply a second with this constant to get a microsecond.
+ */
 #define MICRO            0.000001
+
+/**
+ * Number of bits forming a byte.
+ */
 #define BYTE_LENGTH      8
+
+/**
+ * Link delay.
+ */
 #define LINK_DELAY       1
+
+/**
+ * Maximal lenght of queues.
+ */
 #define QUEUE_MAX_MSGS  20
+
+/**
+ * Minimal lenght of queues.
+ */
 #define QUEUE_MIN_MSGS  10
 
-QUEUE	msgQ;
+/**
+ * Queue for messages.
+ */
+QUEUE msgQ;
 
+/**
+ * Message of MAX_MESSAGE_SIZE.
+ */
 char msg[MAX_MESSAGE_SIZE];
 
+/**
+ * Is the timer started or not.
+ */
 bool timer_started;
 
-//calcute how long a frame needs to travel over the link
+/**
+ * Returns the transmission delay.
+ *
+ * The transmission delay is the lenght of the message divided by the bandwidth
+ * of the link.
+ *
+ * @param Length the lenght of the message.
+ * @param Link the link to send the message over.
+ * @return Returns the calculated transmission delay.
+ */
 double transmission_delay(size_t length, int link)
 {
   double bandwidth = (double) linkinfo[link].bandwidth * MICRO;
@@ -24,7 +70,18 @@ double transmission_delay(size_t length, int link)
   return transmission_delay;
 }
 
-void transmit(int	link)
+/**
+ * Writes a message on a physical link if possible.
+ *
+ * Sends the first message of the queue over link <code>link</code> if the
+ * physical layer is not busy and starts the timer <code> EV_TIMER1</code>.
+ * If the link is still busy the timer is started for a short time to wait
+ * until the link is ready again.
+ * Additionally the application is enabled if the queue has free space.
+ *
+ * @param link The link to send messages over.
+ */
+void transmit(int link)
 {
   char *msg;
   size_t length;
@@ -53,6 +110,15 @@ void transmit(int	link)
   }
 }
 
+/**
+ * aplication_ready() event-handler.
+ *
+ * Handler is called when the application is ready to create a new message.
+ * It reads a new message from the application and adds it to the queue. If the
+ * queue is full the application is disabled. If no message is under transmission
+ * (timer is not started) <code>transmit()</code> is called to transmit the next
+ * message.
+ */
 static EVENT_HANDLER(application_ready)
 {
   CnetAddr destaddr;
@@ -69,6 +135,13 @@ static EVENT_HANDLER(application_ready)
   }
 }
 
+/**
+ * link_ready() event-handler.
+ *
+ * It is a timer which counts how long a message needs to be transmitted over
+ * a link and then calls the <code>transmit()</code> methode to send the next
+ * message.
+ */
 static EVENT_HANDLER(link_ready)
 {
   int link = 1;
@@ -76,6 +149,12 @@ static EVENT_HANDLER(link_ready)
   transmit(link);
 }
 
+/**
+ * physical_ready() event-handler.
+ *
+ * It is called when the physical layer is ready. It simply reads from the
+ * physical layer and passes the message to the application.
+ */
 static EVENT_HANDLER(physical_ready)
 {
   size_t length;
@@ -87,6 +166,12 @@ static EVENT_HANDLER(physical_ready)
   CHECK(CNET_write_application(msg, &length));
 }
 
+/**
+ * reboot_node() event-handler.
+ *
+ * It is always the first function to be called by the cnet schedule.
+ * Handlers are set, application is enabled.
+ */
 EVENT_HANDLER(reboot_node)
 {
     CHECK(CNET_set_handler(EV_APPLICATIONREADY, application_ready, 0));
