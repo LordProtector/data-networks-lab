e2d59055385b5af4caec5e70eaea484925e13a6d
stombers <s9sttomb@stud.uni-saarland.de>
2011-07-11 11:24:08 +0200

    datatypes.h filled (first approach)

--- datatypes.h
+++ datatypes.h
@@ -8,31 +8,47 @@ struct message
 
 struct segment_header
 {
+  unsigned int id;
+  short ordering;
+  bool isfirst;
+  unsigned int ackid;
+  short ackordering;  
 }
 
 struct segment
 {
   struct segment_header header;
+  char* payload;
 }
 
 /* Data structures for network layer. */
 struct datagram_header
 {
+  CnetAddr srcaddr;
+  CnetAddr destaddr;
+  char hoplimit;
+  bool routing; //upper layer: 1 = routing protocol, 0 = network protocol
 }
 
 struct datagram 
 {
   struct datagram_header header;
+  char* payload;
 }
 
 /* Data structure for link layer. */
 
 struct frame_header
 {
-
+  size_t length;
+  int checksum;
+  unsigned int id;
+  short ordering;
+  bool isfirst;
 }
 
 struct frame
 {
   struct frame_header header;
+  char* payload;
 }
