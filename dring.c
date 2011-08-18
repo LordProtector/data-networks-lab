4bc934de55b93ef033e0df83db218825d73ccfa3
JonasB <s9jobuer@stud.uni-saarland.de>
2011-08-18 11:09:10 +0200

    Add documentation for dring.c

--- dring.c
+++ dring.c
@@ -1,16 +1,44 @@
+/**
+ * dring.c
+ *  
+ * @autors Stefan Tombers, Alexander Bunte, Jonas Bürse
+ *
+ * Implementation of a double ring.
+ *
+ * It is used for cyclic sequence numbers whereas the number of different sequence
+ * numbers should be at least twice as large as the window size.
+ *
+ * The double ring stores data in two sorted queues. The first queue always
+ * contains the "smaller" elements of the two queues. Elements are added to
+ * the second queue if they are "larger" than elements of the first queue.
+ * This is the case if they are numerically smaller AND the difference to the
+ * largest element of the first queue is larger than the window size.
+ */
+
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <assert.h>
 #include "squeue.h"
 #include "dring.h"
 
+/**
+ * Data structure for the double ring.
+ */
 typedef struct _DRING
 {
-	SQUEUE a;
-	SQUEUE b;
-	int windowSize;
+	SQUEUE a; // queue with the "smaller" elements
+	SQUEUE b; // queue with the "larger" elements
+	int windowSize; // size of the window
 } _DRING;
 
+
+/**
+ * Creates a new double ring.
+ * 
+ * @param windowSize Size of the window.
+ * @return returns the created ring.
+ */
 DRING dring_new(int windowSize)
 {
 	_DRING *dring = malloc(sizeof(*dring));
@@ -20,6 +48,12 @@ DRING dring_new(int windowSize)
 	return (DRING)dring;
 }
 
+
+/**
+ * Frees all resources allocated for the given double ring.
+ * 
+ * @param d Handle of double ring to destroy.
+ */
 void dring_free(DRING d)
 {
 	_DRING *dring = (_DRING *)d;
@@ -28,6 +62,14 @@ void dring_free(DRING d)
 	free(dring);
 }
 
+/**
+ * Insert an element in the double ring.
+ * The data is inserted in the ring with the smallest distance to the existing data.
+ * This ensures that data are handled correctly.
+ *
+ * @param d Handle of the double ring.
+ * @param data The Data to be added to the ring.
+ */
 void dring_insert(DRING d, int data)
 {
 	_DRING *dring = (_DRING *)d;
@@ -41,6 +83,13 @@ void dring_insert(DRING d, int data)
 	}
 }
 
+
+/**
+ * Returns the "smallest" element from the double ring.
+ *
+ * @param d Handle of the double ring.
+ * @return The "smallest" element of the double ring.
+ */
 int dring_pop(DRING d)
 {
 	_DRING *dring = (_DRING *)d;
@@ -63,6 +112,14 @@ int dring_pop(DRING d)
 	}
 }
 
+
+/**
+ * Returns the number of elements of the double ring.
+ * This is the total number of elements of both rings.
+ *
+ * @param d Handle of the double ring.
+ * @return Number of elements of the double ring.
+ */
 int dring_nitems(DRING d)
 {
 	_DRING *dring = (_DRING *)d;
