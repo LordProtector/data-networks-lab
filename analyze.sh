8a9e22353034b098162820d47fd5be5c95425581
JonasB <s9jobuer@stud.uni-saarland.de>
2011-09-12 13:30:47 +0200

    add documentation for parameters in start scripts

--- analyze.sh
+++ analyze.sh
@@ -2,6 +2,10 @@
 
 rm *.o
 mkdir tmp
+
+#$1 = period of execution
+#$2 = topology file
+
 cnet -W -s -T -e $2 -f 1s $1 > tmp/out
 analyze/build/analyze tmp/out tmp/results
 gnuplot tmp/results.gnuplot
--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
 rm *.o
+#$1 = period of execution
+#$2 = topology file
+
 cnet -W -s -T -e $2 $1
