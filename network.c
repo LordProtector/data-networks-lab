f48094b9594f85842b643c5257240fcbe6d9b6f9
JonasB <s9jobuer@stud.uni-saarland.de>
2011-09-22 17:02:45 +0200

    network.c: fix error in weighting function
    additionally multiply it by factor of 10

--- network.c
+++ network.c
@@ -448,8 +448,10 @@ ROUTING_ENTRY *routing_lookup(CnetAddr addr)
 int get_weight(int link)
 {
 	//return 10000000 * 1.0 / link_get_bandwidth(link);
-	double base = (link_get_bandwidth(link)-5.);
-	return 1000000. *  (-0.04*(base * base * base)+6.);
+	double base = (100000. / link_get_bandwidth(link)-5.);
+	double weight = 10. *  (-0.04*(base * base * base)+6.);
+
+	return weight;
 }
 
 
